2f4f2f9e3fba470d1f212deb166f1d015374ef21
Abiodun Segun <84022331+realshegz-github@users.noreply.github.com>
2021-08-31 13:29:05 +0100

    first commit on budget calculator

---
+++ bdc.js
@@ -0,0 +1,27 @@
+// let totalBudget = document.getElementById('tbdg');
+// let fixedAmount = document.getElementById('add-money').innerText;
+// totalBudget.appendChild(fixedAmount);
+
+// function myChangeFunction(totalBudget) {
+//     var fixedAmount = document.getElementById('add-money');
+//     fixedAmount.value = totalBudget.value;
+// }
+
+// document.querySelector("button").addEventListener("click", handleAddMoney)
+// function handleAddMoney() {
+//     let collector = document.getElementById('add-money').innerText
+//     let totalBudget = document.createElement('p')
+//     totalBudget.textContent = collector
+
+// }
+
+//const fixedAmount = document.getElementById('#add-money').value;
+//const moneyBtn = document.getElementById('addBtn');
+
+const fixedMoney = document.getElementById('#fbdg');
+
+//moneyBtn.addEventListener('click', print);
+
+function print() {
+	console.log('I dey listen');
+}
